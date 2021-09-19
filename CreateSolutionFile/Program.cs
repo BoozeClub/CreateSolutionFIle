5ecd5224ae0273b6c06b68b8f5810095d6db3db3
Edwin Handschuh, sr <BoozeClub@live.com>
2021-09-19 17:13:39 -0500

    Init

---
+++ CreateSolutionFile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreateSolutionFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
